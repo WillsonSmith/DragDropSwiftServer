4bf1fadf9f5cd98f4233aa0c9352f7bb0537b88b
Willson Smith <me@willsonsmith.com>
2015-11-24 21:10:55 -0500

    also remove directory js

--- Swift web hybrid template/web_root/js/app.js
+++ Swift web hybrid template/web_root/js/app.js
@@ -2,3 +2,7 @@ function addServerDirectory(directory) {
     var p = document.getElementById("server-directory");
     p.textContent = directory;
 }
+function removeServerDirectory(directory) {
+    var p = document.getElementById("server-directory");
+    p.textContent = "No directory set.";
+}
